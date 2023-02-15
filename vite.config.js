61424571e11474e454a85748feff8f749510c3aa
SergeyKorobka <s.korobka@goit.ua>
2023-02-15 02:29:15 +0000

    add multi-entry

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,15 @@
 import { defineConfig } from 'vite';
-import path from 'path';
-import fs from 'fs';
+import multi from '@rollup/plugin-multi-entry';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-
-const findByExtension = (dir, ext) => {
-  const matchedFiles = [];
-
-  const files = fs.readdirSync(dir);
-
-  for (const file of files) {
-    const fileExt = path.extname(file);
-
-    if (fileExt === `.${ext}`) {
-      matchedFiles.push(`${dir}/${file}`);
-    }
-  }
-
-  return matchedFiles;
-};
 
 export default defineConfig({
   root: 'src',
   build: {
     rollupOptions: {
-      input: findByExtension('./src', 'html'),
+      input: ['./src/**.html'],
     },
     outDir: '../dist',
   },
-  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  plugins: [multi(), injectHTML(), FullReload(['./src/**/**.html'])],
 });
